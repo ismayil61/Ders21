7e74dda9062b17a7b9e54734feaad4f7d3185a05
ismayil61 <ismayilsahane@gmail.com>
2025-12-16 21:24:13 +0400

    ders21

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ function addProduct() {
   let td2 = document.createElement("td");
   td2.innerText = inputs[1].value;
 
-  let td3 = document.createElement("td");
-  let img = document.createElement("img");
-  img.src = inputs[2].value;
-  img.style.width = "100px";
-  td3.append(img);
+ let td3 = document.createElement("td");
+  td3.innerText = inputs[2].value;
 
-  tr.append(td1, td2, td3);
+  let td4 = document.createElement("td");
+  let img = document.createElement("img");
+  img.src = inputs[3].value;
+  img.style.width = "100px";
+  td4.append(img);
+
+  tr.append(td1, td2, td3,td4);
   tbody.append(tr);
 }
